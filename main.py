60a956a67146ff3c2345fd23feaffd2201b30bd0
YourAverageMo <Yilmaz@arizona.edu>
2024-08-14 14:59:44 -0700

    add clip and youtube

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+import subprocess
+from pytube import YouTube
+
+
+def get_link():
+    link = subprocess.getoutput("powershell.exe -Command Get-Clipboard")
+    return link
+
+
+link = "https://www.youtube.com/watch?v=44vfe7D6GkQ"
+
+yt = YouTube(link)
+
+print(yt.title)
