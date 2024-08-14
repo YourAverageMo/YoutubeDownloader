f1282508df2d4ef66805ed67a1f7efb610589283
YourAverageMo <Yilmaz@arizona.edu>
2024-08-14 16:44:21 -0700

    finish readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Youtube Downloader
-simple python script to download youtube videos using pytube
+
+simple personal python script to download youtube videos using ~pytube~ pytubefix.
+
+# Usage
+
+Copy the url of the video you want to download to clipboard then run python script. the script will grab the first item in your clipboard and download the highest quality available for that video.
--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
 
-def get_link():
+def get_link() -> str:
+    """gets last item in users clipboard. first copy youtube video url to clipboard then run app
+
+    Returns:
+        str: 
+    """
     link = subprocess.getoutput("powershell.exe -Command Get-Clipboard")
     return link
 
 
-link = "https://www.youtube.com/watch?v=44vfe7D6GkQ"
+# link = "https://www.youtube.com/watch?v=44vfe7D6GkQ"
 
-
-yt = YouTube(link, on_progress_callback = on_progress)
-
+yt = YouTube(get_link(), on_progress_callback=on_progress)
 yd = yt.streams.get_highest_resolution()
 yd.download("./Downloads")
